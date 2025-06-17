8598c9a8970e2866c375ef61851c1981c7746737
heywinit <heywinit@gmail.com>
2025-06-18 01:08:43 +0530

    try

--- api/index.go
+++ api/index.go
@@ -1,12 +1,28 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
- 
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	programName := os.Getenv("PROGRAM_NAME")
-	fmt.Fprintf(w, "<h1>Hello from Go! %s</h1>", programName)
+	// Create a new Fiber app
+	app := fiber.New()
+
+	// Add a route
+	app.Get("/*", func(c *fiber.Ctx) error {
+		programName := os.Getenv("PROGRAM_NAME")
+		if programName == "" {
+			programName = "Vercel Go with Fiber"
+		}
+		return c.SendString("<h1>Hello from Go Fiber! " + programName + "</h1>")
+	})
+
+	// Convert Fiber app to http.Handler and serve the request
+	handler := adaptor.FiberApp(app)
+	handler.ServeHTTP(w, r)
 }
+
